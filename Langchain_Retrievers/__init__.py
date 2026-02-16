62cdf7266c53ba3e48db2f53dc2d5d421be4297e
pranava03 <82448612+pranava03@users.noreply.github.com>
2026-02-16 11:16:17 +0530

    Langchain Retrievers

---
+++ Langchain_Retrievers/__init__.py
@@ -0,0 +1,7 @@
+"""
+
+Types of retrievers : 
+|-> Based on the "Data Source" : [wikipedia retriever, vector store retriever, arxiv retriever]
+|-> Based on the "search stratergy" : [MaximumMarginalRelevance(MMR), MultiQueryRetriever, ContextualCompressionRetriever]
+
+"""
